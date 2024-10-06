547ebad73546b143212b901171f35fad1577d1cd
BackRunner <dev@backrunner.top>
2024-10-06 10:28:50 +0000

    feat: submit config file

---
+++ src/config/config.ts
@@ -0,0 +1,13 @@
+import { env } from 'node:process';
+
+export const PROXY_CONFIG = {
+	ALLOW_ORIGIN: env.ALLOW_ORIGIN ?? '',
+	PROXY_USER_AGENT: env.PROXY_USER_AGENT ?? '',
+	PROXY_KEY: env.PROXY_KEY ?? '',
+	THIRD_PARTY_CLIENTS_USER_AGENT: env.THIRD_PARTY_CLIENTS_USER_AGENT ? env.THIRD_PARTY_CLIENTS_USER_AGENT || '').split(',').map((item) => item.trim()) : [],
+	VALIDATE_PATHNAME: env.VALIDATE_PATHNAME ? env.VALIDATE_PATHNAME === 'true' : true,
+	VALIDATE_SIGN: env.VALIDATE_SIGN ? env.VALIDATE_SIGN === 'true' : false,
+	VALIDATE_REFERER: env.VALIDATE_REFERER ? env.VALIDATE_REFERER === 'true' : false,
+	RETURN_EMPTY_PIC_WHEN_ERROR: env.RETURN_EMPTY_PIC_WHEN_ERROR ? env.RETURN_EMPTY_PIC_WHEN_ERROR === 'true' : false,
+    MAX_CONTENT_LENGTH: env.MAX_CONTENT_LENGTH ?? 0,
+};
